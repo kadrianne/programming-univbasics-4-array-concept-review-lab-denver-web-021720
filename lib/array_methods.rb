c9249ae52ecc271545ebf3ea2b6d15f111743572
Kristine Du <kristine.a.du@gmail.com>
2020-01-26 01:30:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,7 +7,18 @@ def find_element_index(array, value_to_find)
   nil
 end
 
+# def find_max_value(array)
+#   new_array = array.sort
+#   new_array[-1]
+# end
+
+# def find_min_value(array)
+#   new_array = array.sort
+#   new_array[0]
+# end
+
 def find_max_value(array)
+  array
   new_array = array.sort
   new_array[-1]
 end
